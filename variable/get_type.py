22d7751be13ccc74aeec1bef112742d9d5ce8248
Nopparat1810 <noppharat.w@ku.th>
2021-01-10 14:16:17 +0700

    create_getType_singelOrDouble

---
+++ variable/get_type.py
@@ -0,0 +1,10 @@
+name = "noppharat wongsa"
+age = 21
+weight = 63.0
+can_walk = True
+can_run = False
+print(type(name))
+print(type(age))
+print(type(weight))
+print(type(can_walk))
+print(type(can_run))
