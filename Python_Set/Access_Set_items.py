261d677fcea613e3ff8ed7e2a20fb00d87e6cd33
Nopparat1810 <noppharat.w@ku.th>
2021-01-16 20:28:29 +0700

    python set

---
+++ Python_Set/Access_Set_items.py
@@ -0,0 +1,9 @@
+thislist = ["apple", "banana", "cherry"]
+
+for x in thislist:
+    print(x)
+
+    
+fluits =  {"apple", "banana", "cherry"}
+if "apple" in fluits
+    print("Yes, apple is a fluit!")
