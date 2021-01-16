deec45382555b1221fb51975af6648dc363f16da
Nopparat1810 <noppharat.w@ku.th>
2021-01-16 14:52:51 +0700

    str

---
+++ workshop3_0/string/Modify_String.py
@@ -0,0 +1,8 @@
+str = "hello, World!"
+
+print(string.upper())
+print(string.lower())
+print(string.strip())
+print(string.replace("h", "J"))
+print(string.split(","))
+print(len(string))
---
+++ workshop3_0/string/f-Strings.py
@@ -0,0 +1,6 @@
+name = "jim"
+age = 26
+
+result = f"My name is {name},and I am "
+
+print("result", result)
